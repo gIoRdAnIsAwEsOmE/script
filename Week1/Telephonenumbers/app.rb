46916fcfb605dce07f68724023a52f42b7670207
Giordan Escalona <gescalona@giordanquarters.home>
2017-03-04 15:08:34 -0500

    Fixed conventions, tabs, and 2 spaces

--- Week1/Telephonenumbers/app.rb
+++ Week1/Telephonenumbers/app.rb
@@ -1,14 +1,12 @@
 require "csv"
-badnumbers = File.read("import_telephone_numbers.csv")
+bad_numbers = File.read("import_telephone_numbers.csv")
 File.open("export_telephone_numbers.csv", "w") do |file|
-	badnumbers.each_line do |number|
-		orgnum = number
-		if (number.length == 11)
-			number = "1#{number}"
-		end
-		newnumber = number.gsub(/\D/n, "")
-		goodnumber = "#{newnumber[0]}-#{newnumber[1...4]}-#{newnumber[4...7]}-#{newnumber[7...11]}"
-		puts goodnumber
-		file << "#{orgnum} --> #{goodnumber}".gsub(/\n/, "") + "\n"
-	end
+  bad_numbers.each_line do |line|
+  number = line.gsub(/\D/n, "")
+  if (number.length == 10)
+	number = "1#{number}"
+  end
+	good_number = "#{number[0]}-#{number[1...4]}-#{number[4...7]}-#{number[7...11]}"
+	file << "#{good_number}".gsub(/\n/, "") + "\n"
+  end
 end
