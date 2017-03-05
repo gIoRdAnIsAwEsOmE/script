d41c2aac7250e7ad7fea7bbd91ccbefece5a1a92
Giordan Escalona <gescalona@giordanquarters.home>
2017-03-05 11:08:26 -0500

    Finished telephone numbers

--- Week1/Telephonenumbers/app.rb
+++ Week1/Telephonenumbers/app.rb
@@ -3,10 +3,9 @@ bad_numbers = File.read("import_telephone_numbers.csv")
 File.open("export_telephone_numbers.csv", "w") do |file|
   bad_numbers.each_line do |line|
   number = line.gsub(/\D/n, "")
-  if (number.length == 10)
-	number = "1#{number}"
-  end
+  number = "1#{number}" if number.length == 10
 	good_number = "#{number[0]}-#{number[1...4]}-#{number[4...7]}-#{number[7...11]}"
-	file << "#{good_number}".gsub(/\n/, "") + "\n"
+	"#{good_number}" == good_number
+	file << good_number + "\n"
   end
 end
